ea332907fc766ee74ab889ef9bd6302f227483f7
Shawn <sgiovanni1@yahoo.com>
2022-10-03 00:16:43 -0400

    first commit

---
+++ shopping_cart.py
@@ -0,0 +1,56 @@
+# BUILD A SHOPPING CART PROGRAM WITH THE FOLLOWING CAPABILITES:
+
+# 1 Takes in an input
+# 2 Stores user input into a dictionary or list
+# 3 The User can add or delete items
+# 4 The User can see current shopping list
+# 5 The program loops untiluse quits
+# 6 Upon quitting the program, prints out a recie[t of the items with total and quantity
+
+
+shopping_cart= set()
+item = input('what would you like to add to your cart?')
+
+
+def Items_needed(*):   
+    print('welcome to list right shopping cart, enter items when ready.\n press "D" when your done./npress "e" to edit your cart'
+    )
+
+while True:
+    init_q = input("what would you like to do?")
+    if init_q == "e":
+        break
+    edit = input('to remove and item press "r" \nto exchange an item press "ex"/nto go back press "b"')
+    while True:
+        def exchange("removed","added")
+
+        if edit == "r":
+            r_input = input('what would you like to remove? \npress "b" to go back')
+
+
+            if r_input == "*":
+                shopping_cart.remove(*)
+                print('"*" has been removed.')
+            else:
+
+        elif edit == "ex":
+            ex_input =()
+
+
+
+        break
+    else:shopping_cart.append()
+
+items_needed()
+
+
+
+
+       
+
+
+    
+    
+
+    
+   
